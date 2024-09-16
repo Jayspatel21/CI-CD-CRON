c0c9e879a90c22afc7dd8d63ddeaa5fd604b0b14
jayspatel21 <jay.patel@linearloop.io>
2024-09-16 10:46:34 +0530

    first

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"  # Specify a version or use the latest compatible version
+    }
+  }
+}
+provider "hashicorp/digitalocean" {
+  token = var.digitalocean_token
+}
+
+
+
+resource "digitalocean_droplet" "example" {
+  image  = "ubuntu-20-04-x64"
+  name   = "ci-cd-droplet"
+  region = "nyc3"
+  size   = "s-1vcpu-1gb"
+  
+  # Use cloud-init user data to set default password
+  user_data = <<-EOF
+  #cloud-config
+  chpasswd:
+    list: |
+      root:jack2153
+    expire: False
+  EOF
+}
+
+# Output Droplet IP
+output "droplet_ip" {
+  value = digitalocean_droplet.example.ipv4_address
+}
+
+variable "digitalocean_token" {
+  type = string
+  description = "Your DigitalOcean API Token"
+}
+
