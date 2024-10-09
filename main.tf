9f760eaaf0c14953069165e9466129583b548e35
jayspatel21 <jay.patel@linearloop.io>
2024-10-09 11:35:40 +0530

    ee

--- main.tf
+++ main.tf
@@ -1,49 +1,37 @@
 terraform {
   backend "remote" {
-    organization = "jackkkk"  # Replace with your organization name
+    organization = "jackkkk"
 
     workspaces {
-      name = "Jackk-1"  # Replace with your workspace name
+      name = "Jackk-1"
     }
   }
-
 
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.0"  # Specify a version or use the latest compatible version
+      version = "~> 2.0"
     }
   }
 }
+
 provider "digitalocean" {
   token = var.digitalocean_token
 }
-
-
 
 resource "digitalocean_droplet" "example" {
   image  = "ubuntu-20-04-x64"
   name   = "ci-cd-droplet"
   region = "blr1"
   size   = "s-1vcpu-1gb"
-  
-  # Use cloud-init user data to set default password
-  user_data = <<-EOF
-  #cloud-config
-  chpasswd:
-    list: |
-      root:jack2153
-    expire: False
-  EOF
 }
 
-# Output Droplet IP
 output "droplet_ip" {
   value = digitalocean_droplet.example.ipv4_address
 }
 
 variable "digitalocean_token" {
-  type = string
+  type        = string
   description = "Your DigitalOcean API Token"
+  sensitive   = true  # Added sensitive flag for security
 }
-
